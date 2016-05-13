1bcca8f7eda2e6c73103c18c1c24a34360c243c8
Matt Miller <mmiller@helixeducation.com>
2016-05-13 14:09:41 -0600

    Make deploy script only redeploy when master is pushed

--- deploy.php
+++ deploy.php
@@ -6,7 +6,17 @@ if (
 	hash_equals($_SERVER['HTTP_X_HUB_SIGNATURE'], verify_request($body))
 ) {
 	header('HTTP/1.1 403 Forbidden');
-	die('Invalid request.');
+	die('Request did not originate from GitHub-Hookshot with the correct secret.');
+}
+
+try {
+	$input = json_parse(file_get_contents('php://input'));
+	if ($input->ref !== 'refs/heads/master') {
+		die('Push was not to master branch. Canceling deploy.');
+	}
+} catch (Exception $ex) {
+	header('HTTP/1.1 400 Bad Request');
+	die('Body of request could not be parsed as JSON or was missing `ref` property.');
 }
 
 foreach (array('git pull origin master') as $command) {
@@ -29,4 +39,4 @@ function hash_equals ($a, $b) {
 	return $result === 0;
 }
 
-die('Deployment successful.');
+die('Deployment of master branch was successful.');
